d2f3b3c44d22d9b209bf7e9d5ecb91efc5aff849
programminghusky <husky2020coding@gmail.com>
2020-11-01 14:32:47 -0800

    Maximum Subarray with the largest sum

---
+++ LeetCodeQuestions/Easy/MaximumSubarray.cs
@@ -0,0 +1,25 @@
+﻿/*Given an integer array nums, find the contiguous subarray (containing at least one number) 
+ * which has the largest sum and return its sum. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeQuestions.Easy
+{
+    class MaximumSubarray
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int maxSoFar = nums[0], maxEndingHere = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                maxEndingHere = Math.Max(nums[i], maxEndingHere + nums[i]);
+                maxSoFar = Math.Max(maxSoFar, maxEndingHere);
+            }
+            return maxSoFar;
+        }
+    }
+}
