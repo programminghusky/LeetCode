68676489eaa46ebd2376c4599ff82c7e880f5443
programminghusky <husky2020coding@gmail.com>
2020-11-01 14:24:32 -0800

    Two number sum to target

---
+++ LeetCodeQuestions/Easy/TwoSum.cs
@@ -0,0 +1,31 @@
+﻿/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeQuestions.Easy
+{
+    class TwoSum
+    {
+        public int[] TwoNumberSum(int[] nums, int target)
+        {
+            Dictionary<int, int> numHash = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int secondNum = target - nums[i];
+                if (!numHash.ContainsKey(secondNum))
+                {
+                    if (!numHash.ContainsKey(nums[i]))
+                        numHash.Add(nums[i], i);
+                }
+                else
+                {
+                    return new int[] { numHash[secondNum], i };
+                }
+            }
+            return new int[] { -1, -1 };
+        }
+    }
+}
--- LeetCodeQuestions/Program.cs
+++ LeetCodeQuestions/Program.cs
@@ -10,6 +10,7 @@ namespace LeetCodeQuestions
     {
         static void Main(string[] args)
         {
+            Console.ReadLine();
         }
     }
 }
