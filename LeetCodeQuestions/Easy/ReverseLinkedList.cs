dbb624b38b6268b736401ab0f002b2295b940811
programminghusky <husky2020coding@gmail.com>
2020-11-02 09:02:48 -0800

    Reverse singly linked list

---
+++ LeetCodeQuestions/Easy/ReverseLinkedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeQuestions.Easy
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    class ReverseLinkedList
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            // prev <- next <- head
+            ListNode prev = null;
+
+            while (head != null)
+            {
+                ListNode next = head.next;
+                head.next = prev;
+                prev = head;
+                head = next;
+            }
+            return prev;
+        }
+    }
+}
