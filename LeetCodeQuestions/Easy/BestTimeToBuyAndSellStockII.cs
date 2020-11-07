f9ef93f021710485359ba97a31d99d15df8946ed
programminghusky <husky2020coding@gmail.com>
2020-11-07 15:07:46 -0800

    Best time to buy and sell stock - can complete multiple transactions but not at the same time

---
+++ LeetCodeQuestions/Easy/BestTimeToBuyAndSellStockII.cs
@@ -0,0 +1,27 @@
+﻿/* Say you have an array prices for which the ith element is the price of a given stock on day i.
+ * Design an algorithm to find the maximum profit. You may complete as many transactions as you like 
+ * (i.e., buy one and sell one share of the stock multiple times). */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeQuestions.Easy
+{
+    class BestTimeToBuyAndSellStockII
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > prices[i - 1])
+                {
+                    maxProfit += (prices[i] - prices[i - 1]);
+                }
+            }
+            return maxProfit;
+        }
+    }
+}
