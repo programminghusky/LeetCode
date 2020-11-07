b8127704114785ec2801810cd1464e8d23451cc3
programminghusky <husky2020coding@gmail.com>
2020-11-07 14:56:50 -0800

    Every element appears twice except one

---
+++ LeetCodeQuestions/Easy/SingleNumber.cs
@@ -0,0 +1,22 @@
+﻿/* Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeQuestions.Easy
+{
+    class SingleNumber
+    {
+        public int SingleNumber1(int[] nums)
+        {
+            int xor = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                xor = xor ^ nums[i];
+            }
+            return xor;
+        }
+    }
+}
