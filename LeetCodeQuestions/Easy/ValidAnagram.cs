7ecea4aa9facfcb20f671c0b8248384d7469aaea
programminghusky <husky2020coding@gmail.com>
2020-11-07 14:59:38 -0800

    Valid Anagram

---
+++ LeetCodeQuestions/Easy/ValidAnagram.cs
@@ -0,0 +1,32 @@
+﻿/* Given two strings s and t , write a function to determine if t is an anagram of s. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeQuestions.Easy
+{
+    class ValidAnagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            int[] intArray = new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                intArray[s[i] - 'a']++;
+            }
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                intArray[t[i] - 'a']--;
+            }
+            for (int i = 0; i < 26; i++)
+            {
+                if (intArray[i] != 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
