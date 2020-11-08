932a4a37447f5ff58d0d11131ee8df7c4e254085
programminghusky <husky2020coding@gmail.com>
2020-11-07 23:14:45 -0800

    Add two numbers which are given as Linked Lists stored in reverse order

---
+++ LeetCodeQuestions/Medium/AddTwoNumbers.cs
@@ -0,0 +1,51 @@
+﻿/* You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order,
+ * and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeQuestions.Medium
+{
+    class AddTwoNumbers
+    {
+        public ListNode AddTwoNumbers1(ListNode l1, ListNode l2)
+        {
+            ListNode dummyHead = new ListNode(0);
+            ListNode p = l1, q = l2, curr = dummyHead;
+            int carry = 0;
+
+            while(p!= null || q!= null)
+            {
+                int x = (p != null) ? p.val : 0;
+                int y = (q != null) ? q.val : 0;
+                int sum = x + y + carry;
+                carry = sum / 10;
+                curr.next = new ListNode(sum % 10);
+                curr = curr.next;
+
+                if (p != null)
+                    p = p.next;
+                if (q != null)
+                    q = q.next;
+            }
+            if (carry > 0)
+                curr.next = new ListNode(carry);
+
+            return dummyHead.next;
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}
