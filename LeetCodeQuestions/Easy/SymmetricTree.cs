f7d6b923675e1c08406164774753f6c12104016a
husky <husky2020coding@gmail.com>
2021-02-19 15:19:30 -0800

    Check whether a tree is mirror of itself

---
+++ LeetCodeQuestions/Easy/SymmetricTree.cs
@@ -0,0 +1,42 @@
+﻿/*  Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeQuestions.Easy
+{
+    class SymmetricTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null)
+                return true;
+            return Helper(root.left, root.right);
+        }
+
+        public bool Helper(TreeNode node1, TreeNode node2)
+        {
+            if (node1 == null && node2 == null)
+                return true;
+            if (node1 == null || node2 == null)
+                return false;
+            if (node1.val != node2.val)
+                return false;
+            return Helper(node1.left, node2.right) && Helper(node1.right, node2.left);
+        }
+    }
+}
