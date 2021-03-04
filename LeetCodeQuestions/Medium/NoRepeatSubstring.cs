4842be20d48e84c413f151d862adbc1ae66d3cc2
husky <husky2020coding@gmail.com>
2021-03-03 17:46:16 -0800

    another way to do no repeat substring

--- LeetCodeQuestions/Medium/NoRepeatSubstring.cs
+++ LeetCodeQuestions/Medium/NoRepeatSubstring.cs
@@ -12,25 +12,43 @@ namespace LeetCodeQuestions.Medium
     {
         public int LengthOfLongestSubstring(string s)//abbcd// a b in hashset
         {
+            //int maxLength = 0, windowStart = 0;
+            //HashSet<char> freqSet = new HashSet<char>();
+            //for (int i = 0; i < s.Length; i++)
+            //{
+
+            //    if (!freqSet.Contains(s[i]))
+            //        freqSet.Add(s[i]);
+            //    else
+            //    {
+            //        //do untill removed
+            //        while(freqSet.Contains(s[i]))
+            //        {
+            //            freqSet.Remove(s[windowStart]);
+            //            windowStart++;
+            //        }
+            //        freqSet.Add(s[i]);
+            //    }
+            //     maxLength = Math.Max(maxLength, i - windowStart + 1);
+            //}
+            //return maxLength;
+
             int maxLength = 0, windowStart = 0;
-            HashSet<char> freqSet = new HashSet<char>();
+            Dictionary<char,int> freqMap = new Dictionary<char, int>();
             for (int i = 0; i < s.Length; i++)
             {
-
-                if (!freqSet.Contains(s[i]))
-                    freqSet.Add(s[i]);
+                if (!freqMap.ContainsKey(s[i]))
+                {
+                    freqMap.Add(s[i], i);
+                }
                 else
                 {
-                    //do untill removed
-                    while(freqSet.Contains(s[i]))
-                    {
-                        freqSet.Remove(s[windowStart]);
-                        windowStart++;
-                    }
-                    freqSet.Add(s[i]);
+                    windowStart = Math.Max(windowStart, freqMap[s[i]] + 1);
+                    freqMap[s[i]] = i; 
                 }
-                 maxLength = Math.Max(maxLength, i - windowStart + 1);
+                maxLength = Math.Max(maxLength, i - windowStart + 1);
             }
+
             return maxLength;
         }
     }
