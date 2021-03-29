b3a712d4f27309dc23cc52489987e39f5b76a4ff
husky <husky2020coding@gmail.com>
2021-03-29 07:52:24 -0700

    Maximum Consecutive Ones

---
+++ LeetCodeQuestions/Medium/MaxConsecutiveOnes.cs
@@ -0,0 +1,32 @@
+﻿//https://leetcode.com/problems/max-consecutive-ones-iii/
+//Given an array A of 0s and 1s, we may change up to K values from 0 to 1.
+//Return the length of the longest(contiguous) subarray that contains only 1s.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeQuestions.Medium
+{
+    class MaxConsecutiveOnes
+    {
+        public int LongestOnes(int[] A, int K)
+        {
+            int windowStart = 0, maxLength = 0, maxOnesCount = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == 1)
+                    maxOnesCount++;
+                if (i - windowStart + 1 - maxOnesCount > K)
+                {
+                    if (A[windowStart] == 1)
+                        maxOnesCount--;
+                    windowStart++;
+                }
+                maxLength = Math.Max(i - windowStart + 1, maxLength);
+            }
+            return maxLength;
+        }
+    }
+}
